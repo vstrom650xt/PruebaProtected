d955265e4d6a12d92356ad4c17003734bf591f61
vstrom650xt <cervermonzo@gmail.com>
2023-02-21 13:53:03 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import pp.Lel;
+
+public class Main {
+    public static void main(String[] args) {
+        Lel p = new Lel("jose");
+
+    }
+}
---
+++ src/pp/Lel.java
@@ -0,0 +1,17 @@
+package pp;
+
+public class Lel {
+    protected   String nombre;
+
+    public Lel(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Lel{" +
+                "nombre='" + nombre + '\'' +
+                '}';
+    }
+
+}
